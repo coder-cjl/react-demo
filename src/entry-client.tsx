1a6cee4c27fe19dea4716c573435d29426d10ad1
雷子 <cjlsire@126.com>
2025-11-07 11:30:09 +0800

    feat: 更新入口文件，重构客户端和服务器渲染逻辑

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,9 +1,19 @@
-import { RouterProvider } from 'react-router-dom'
-import { router } from './routers'
-import './App.css'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { isDev } from './utils/env.ts'
+import VConsole from 'vconsole'
+import { logger } from './utils/log.ts'
 
-function App() {
-  return <RouterProvider router={router} />
+/// 仅仅在开发模式下引入
+if (isDev) {
+  const vconsole = new VConsole()
+  logger.debug('VConsole is enabled in development mode.', vconsole)
 }
 
-export default App
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
--- src/entry-srver.tsx
+++ src/entry-srver.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { renderToString } from 'react-dom/server'
 import App from './App'
 
-export function render(url: string) {
+export function render() {
   const html = renderToString(
     <StrictMode>
       <App />
