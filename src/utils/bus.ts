5f35fbc64b8b5e9a0d0c203bb52f5f71ee34f13c
雷子 <cjlsire@126.com>
2025-11-12 16:21:52 +0800

    feat: 添加事件总线实现，支持事件的注册、注销和触发

---
+++ src/utils/bus.ts
@@ -0,0 +1,34 @@
+type EventCallback = (...args: unknown[]) => void
+
+class LucaBus {
+  private events: { [key: string]: EventCallback[] } = {}
+
+  on(eventName: string, callback: EventCallback) {
+    if (!this.events[eventName]) {
+      this.events[eventName] = []
+    }
+    this.events[eventName].push(callback)
+  }
+
+  off(eventName: string, callback?: EventCallback) {
+    if (!this.events[eventName]) return
+
+    if (callback) {
+      this.events[eventName] = this.events[eventName].filter(
+        cb => cb !== callback
+      )
+    } else {
+      delete this.events[eventName]
+    }
+  }
+
+  emit(eventName: string, ...args: unknown[]) {
+    if (!this.events[eventName]) return
+
+    this.events[eventName].forEach(callback => {
+      callback(...args)
+    })
+  }
+}
+
+export const eventBus = new LucaBus()
